7f5805075209623af8fe2884ef6662f6851fd78c
michalnov <michalnov3@gmail.com>
2019-01-17 17:48:03 +0100

    update

--- bin/server/modules/persistance/persistance.go
+++ bin/server/modules/persistance/persistance.go
@@ -1,9 +1,17 @@
 package persistance
 
-type persistance struct {
+import (
+	"github.com/michalnov/SovyGo/bin/server/modules/authentication"
+)
+
+//Persistance struct
+type Persistance struct {
+	state map[string]authentication.Token
 }
 
 //NewPersistance create new persistance structure
-func NewPersistance() {
-
+func NewPersistance() Persistance {
+	out := Persistance{}
+	out.state = make(map[string]authentication.Token, 0)
+	return out
 }
