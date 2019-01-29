9e56d9fc4e6ff4e7d64dcb33225ff54e8598b130
michalnov <michalnov3@gmail.com>
2019-01-29 12:24:06 +0100

    .

--- install/apache/js/mainSec.js
+++ install/apache/js/mainSec.js
@@ -62,7 +62,8 @@ function importPublicKey(data) {
             let env = new Envelop;
             env.encryption = false;
             env.fromEnvelop(this.responseText);
-            state.scrypt.serverPub = pki.publicKeyFromPem(env.key.split("&#xA;").join("\n"));
+            let x = String(env.key)
+            state.scrypt.serverPub = pki.publicKeyFromPem(x.split("&#xA;").join("\n"));
             reportState("imported");
             sendAESKey();
         } else {
