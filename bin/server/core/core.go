4598398089cdde3e29a210564a9fd7c5167ac71d
michalnov <michalnov3@gmail.com>
2019-01-30 16:29:25 +0100

    added templates

--- bin/server/core/core.go
+++ bin/server/core/core.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"html/template"
+	"net/http"
+	"path/filepath"
 
 	auth "github.com/michalnov/SovyGo/bin/server/modules/authentication"
 	conf "github.com/michalnov/SovyGo/bin/server/modules/configuration"
@@ -34,20 +36,42 @@ func NewCore() (Core, error) {
 func (c *Core) loadTemplates() error {
 	var err error
 	swap := c.Templates
-	t, err := template.ParseFiles("index.html")
-	checkErr(err)
-	tmpl := template.Must(t, err)
-	checkErr(err)
-	swap["index"] = tmpl
-	t, err = template.ParseFiles("login.html")
-	checkErr(err)
-	tmpl = template.Must(t, err)
-	checkErr(err)
-	swap["login"] = tmpl
+
+	swap["index"], err = laodTemplate("index.html")
+	if err != nil {
+		return err
+	}
+	swap["login"], err = laodTemplate("login.html")
+	if err != nil {
+		return err
+	}
+	swap["register"], err = laodTemplate("register.html")
+	if err != nil {
+		return err
+	}
+	swap["register"], err = laodTemplate("register.html")
 	if err != nil {
 		return err
 	}
 	c.Templates = swap
 
 	return nil
+}
+
+func laodTemplate(path string) (*template.Template, error) {
+	absPath, _ := filepath.Abs("build/web_files/" + path)
+	t, err := template.ParseFiles(absPath)
+	if err != nil {
+		return nil, err
+	}
+	tmpl := template.Must(t, err)
+	if err != nil {
+		return nil, err
+	}
+	return tmpl, err
+}
+
+//HomeHandler serve main htm page
+func (c *Core) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	c.Templates["index"].Execute(w, nil)
 }
