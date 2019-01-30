168c5a79c9edbe701cc19f6a3f75d3660f2927d0
michalnov <michalnov3@gmail.com>
2019-01-30 20:54:54 +0100

    .

--- bin/server/core/core.go
+++ bin/server/core/core.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"path/filepath"
@@ -27,29 +28,33 @@ func NewCore() (Core, error) {
 	var core Core
 	var err error
 	core.Config, err = conf.ReadConfig()
+	core.loadTemplates()
 	if err != nil {
-		return core, err
+		fmt.Printf("error while loading")
+		panic(err)
+		//return core, err
 	}
 	return core, nil
 }
 
 func (c *Core) loadTemplates() error {
 	var err error
-	swap := c.Templates
+	c.Templates = make(map[string]*template.Template, 0)
+	var swap = make(map[string]*template.Template, 0)
 
-	swap["index"], err = laodTemplate("index.html")
+	swap["index"] = laodTemplate("index.html")
 	if err != nil {
 		return err
 	}
-	swap["login"], err = laodTemplate("login.html")
+	swap["login"] = laodTemplate("login.html")
 	if err != nil {
 		return err
 	}
-	swap["register"], err = laodTemplate("register.html")
+	swap["register"] = laodTemplate("register.html")
 	if err != nil {
 		return err
 	}
-	swap["register"], err = laodTemplate("register.html")
+	swap["test"] = laodTemplate("test.html")
 	if err != nil {
 		return err
 	}
@@ -58,22 +63,23 @@ func (c *Core) loadTemplates() error {
 	return nil
 }
 
-func laodTemplate(path string) (*template.Template, error) {
-	absPath, _ := filepath.Abs("build/web_files/" + path)
-	t, err := template.ParseFiles(absPath)
+func laodTemplate(path string) *template.Template {
+	absPath, err := filepath.Abs("build/web_files/" + path)
+	//fmt.Println(absPath)
+	tmpl := template.Must(template.ParseFiles(absPath))
 	if err != nil {
-		return nil, err
+		fmt.Println("EEE 22")
+		return nil
 	}
-	tmpl := template.Must(t, err)
-	if err != nil {
-		return nil, err
-	}
-	return tmpl, err
+	//fmt.Println("iii")
+	return tmpl
 }
 
 //HomeHandler serve main htm page
 func (c *Core) HomeHandler(w http.ResponseWriter, r *http.Request) {
-	c.Templates["index"].Execute(w, nil)
+	fmt.Println("EEE 110")
+	//http.ServeFile(w, r, "web_files/test.html")
+	c.Templates["index"].Execute(w, "")
 }
 
 //LoginHandler serve main htm page
